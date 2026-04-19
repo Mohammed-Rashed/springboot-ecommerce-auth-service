2f94b0934b0cd23b0c54810cff69f68b34988c2d
rashed <mohammed.rashed@gmail.com>
2026-04-19 14:05:50 +0300

    create User entity and map it to users table

---
+++ src/main/java/com/rashed/ecommerce/auth/user/Role.java
@@ -0,0 +1,6 @@
+package com.rashed.ecommerce.auth.user;
+
+public enum  Role {
+    CUSTOMER,
+    ADMIN
+}
---
+++ src/main/java/com/rashed/ecommerce/auth/user/User.java
@@ -0,0 +1,39 @@
+package com.rashed.ecommerce.auth.user;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
