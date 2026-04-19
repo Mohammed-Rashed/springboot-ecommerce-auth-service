93b9a76d402b6bae5edf04d12139e73114048b2a
rashed <mohammed.rashed@gmail.com>
2026-04-19 15:50:52 +0300

    extend jwt service with token parsing and validation methods

--- src/main/java/com/rashed/ecommerce/auth/security/jwt/JwtService.java
+++ src/main/java/com/rashed/ecommerce/auth/security/jwt/JwtService.java
@@ -1,6 +1,7 @@
 package com.rashed.ecommerce.auth.security.jwt;
 
 import com.rashed.ecommerce.auth.user.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,8 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
+import java.util.function.Function;
+
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
@@ -34,5 +37,30 @@ public class JwtService {
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    public boolean isTokenValid(String token, String email) {
+        String username = extractUsername(token);
+        return username.equalsIgnoreCase(email) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        Date expiration = extractClaim(token, Claims::getExpiration);
+        return expiration.before(new Date());
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
 }
 
